4c7a511eb2271a534a0d9d8dfe0915512a917a9e
Rithish24-cloud <rithishkumar2405@gmail.com>
2025-07-19 00:02:59 +0530

    all files for ec2

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region     = "ap-south-1a"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu 22.04 in us-east-1
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  tags = {
+    Name = "Terraform-DevOps-EC2"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "key_name" {
+  default = "kuber.pem" # replace with your actual EC2 key pair name
+}
