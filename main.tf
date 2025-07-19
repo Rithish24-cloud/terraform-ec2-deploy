f2fbb527b53dcee8b0e4c0e06825ceb2708ef984
Rithish24-cloud <rithishkumar2405@gmail.com>
2025-07-19 17:38:40 +0530

    updated main

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+  key_name      = "kuber.pem"
+
+  tags = {
+    Name = "Simple-EC2-From-Terraform"
+  }
+}
